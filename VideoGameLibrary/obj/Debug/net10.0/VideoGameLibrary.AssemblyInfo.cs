ebd6249713b49bb07d4659582a1929f8157281bf
MattFenix1 <bjduffy@email.neit.edu>
2026-09-08 20:16:50 -0400

    Week 7 Assignment

--- VideoGameLibrary/obj/Debug/net10.0/VideoGameLibrary.AssemblyInfo.cs
+++ VideoGameLibrary/obj/Debug/net10.0/VideoGameLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d75b4464eef2ef63eda96a95c63415c24d1b833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a56fde90a679785f46f587ea74eee4a53ade7e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
